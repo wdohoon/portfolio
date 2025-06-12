21ce77e42fddee29e57810c93a4447aa97bf34d1
Dohoon Won <129389793+wdohoon@users.noreply.github.com>
2025-06-12 13:26:36 +0900

    feat: restructure portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import nextI18NextConfig from './next-i18next.config.js'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: nextI18NextConfig.i18n,
+};
 
 export default nextConfig;
